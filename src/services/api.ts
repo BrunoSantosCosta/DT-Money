ad50fec1ae701cf457e01d4170314973263ef3e9
BrunoSantosCosta <brunosanttos667@gmail.com>
2022-06-05 18:07:34 -0300

    How deploy app in Netlify

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api', For use app the local machine
+  baseURL: 'https://bruno-santos-dtmoney.netlify.app/',
 })
